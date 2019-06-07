e339907bacb5c9fe19e1b4bfda22a083898a4078
valentin <valentin.hamet@gmail.com>
2019-06-07 15:32:47 +0200

    Schemas & Resolvers

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World !');
-});
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Rammstein - La Défense Arena']
+    },
+    createEvent: (args) => {
+      const { name } = args;
+      return name;
+    }
+  },
+  graphiql: true
+}));
 
 app.listen(4000);
