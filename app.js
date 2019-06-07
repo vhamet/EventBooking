2c28c8e591ee10a75436298c5f2999b54756b42d
valentin <valentin.hamet@gmail.com>
2019-06-07 16:44:42 +0200

    GraphQL + MongoDB

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
-
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
@@ -41,22 +41,44 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return events;
+      return Event
+        .find()
+        .then(events => {
+          console.log(events);
+          return events.map(event => ({ ...event._doc, _id: event.id }));
+        })
+        .catch(err => {
+          console.log(err);
+          throw err;
+        });
     },
     createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: new Date().toISOString()
-      };
-      events.push(event);
-
-      return event;
+        date: new Date(args.eventInput.date)
+      });
+      return event
+        .save()
+        .then(result => {
+          console.log(result);
+          return { ...result._doc, _id: event.id };
+        })
+      .catch(err => {
+        console.log(err);
+        throw err;
+      });
     }
   },
   graphiql: true
 }));
 
-app.listen(4000);
+mongoose
+  .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-dpvze.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+  .then(() => {
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
