87355518d6bf7d2dfd1d0a1844ddf4bd16232f84
valentin <valentin.hamet@gmail.com>
2019-06-07 15:51:53 +0200

    Types & Data

--- app.js
+++ app.js
@@ -2,18 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+
 const app = express();
+
+const events = [];
 
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -23,11 +41,19 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return ['Rammstein - La Défense Arena']
+      return events;
     },
     createEvent: (args) => {
-      const { name } = args;
-      return name;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date().toISOString()
+      };
+      events.push(event);
+
+      return event;
     }
   },
   graphiql: true
