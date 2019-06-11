a959f2cc469e18ffa9c9ff450466b8eea7b28a3e
valentin <valentin.hamet@gmail.com>
2019-06-11 16:05:16 +0200

    React frontend setup

---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EventsPage extends Component {
+  render() {
+    return (
+      <h1>The EventsPage Page</h1>
+    );
+  }
+}
+
+export default EventsPage;
