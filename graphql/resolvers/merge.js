21facedfc4d2badca2acddc8e28849915da6c5a3
valentin <valentin.hamet@gmail.com>
2019-06-13 16:36:52 +0200

    bug fixing 2

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -15,15 +15,12 @@ const userLoader = new DataLoader((userIds) => {
 
 const events = async eventIds => {
   try {
-
     const events = await Event.find({ _id: {$in: eventIds} });
+    events.sort((a,b) => {
+      return eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString());
+    });
     return events.map(event => {
-      return { 
-        ...event._doc, 
-        _id: event.id, 
-        date: dateToString(event._doc.date),
-        creator: user.bind(this, event.creator) 
-      };
+      return transformEvent(event);
     });
   } catch(err) {
     throw err;
